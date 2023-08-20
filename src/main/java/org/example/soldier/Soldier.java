17d3e510311b3369eb8678d78e1e8d5d3741d29d
Andrea103 <andreasierra103@gmail.com>
2023-08-20 12:10:19 +0200

    solution of some errors

--- src/main/java/org/example/soldier/Soldier.java
+++ src/main/java/org/example/soldier/Soldier.java
@@ -1,4 +1,20 @@
 package org.example.soldier;
 
 public class Soldier {
+    public int health;
+    public int strength;
+
+    public Soldier(int health, int strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    public int attack(int damage) {
+        return this.strength;
+    }
+
+    public int receiveDamage(int damage) {
+        return this.health -= damage;
+    }
+
 }
